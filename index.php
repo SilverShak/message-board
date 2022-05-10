20394b355a6406a036317a50ddeffc18fde6fc8c
SilverShak <71289768+SilverShak@users.noreply.github.com>
2022-05-10 22:06:24 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
 </div>
 
 <div class="headerArea">
-<span class="headerTitle">הודעות<span>
+<span class="headerTitle">Messages<span>
 </div>
 <hr>
 <button id="messageAdd" href="#" class="btn">הוספה</button>
 &nbsp;&nbsp;
 <input type="checkbox" id="get_all">
-<label for="get_all">כל ההודעות</label>
+<label for="get_all">All messages</label>
 
 <div id="messageArea"> </div>
 
@@ -30,25 +30,25 @@
 
 	<input type="hidden" id="editID">
 	
-	<label for="editCategory">קטגוריה:</label>
+	<label for="editCategory">Category:</label>
 	<select id="editCategory">
-		<option value="כללי">כללי</option>
-		<option value="NSOC">NSOC</option>
+		<option value="General">General</option>
+		<option value="Personal">Personal</option>
 	</select>
 	<br>
 	
-    <label for="editMessage"><b>הודעה</b></label>
+    <label for="editMessage"><b>Message</b></label>
     <br>
 	<pre><textarea id="editMessage" rows="7" cols="auto" form="formEdit" autocomplete="off" maxlength="350" required> </textarea></pre>
 	<br>
-	<label for="editExpiration">תאריך תפוגה:</label>
+	<label for="editExpiration">Expiration:</label>
 	<input type="date" id="editExpiration" placeholder="dd/mm/yyyy" required><br>
 	
-	<label for="editAuthor"><b>שם הכותב</b></label>
+	<label for="editAuthor"><b>Name</b></label>
     <input type="text" id="editAuthor" autocomplete="off" maxlength="15" required>
 	
-    <button type="submit" id="editSave" href="#" class="btn" onsubmit="return false">שמירה</button>
-    <button type="button" id="editClose" class="btn cancel">ביטול</button>
+    <button type="submit" id="editSave" href="#" class="btn" onsubmit="return false">Submit</button>
+    <button type="button" id="editClose" class="btn cancel">Cancel</button>
   </form>
 </div>
 
